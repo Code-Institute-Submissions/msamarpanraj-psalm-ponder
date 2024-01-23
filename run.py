5478589f0c5cf4ce1caaef8da72a4aaff28d6aa0
msamarpanraj <>
2024-01-23 00:53:40 +0000

    Update requirements file

--- run.py
+++ run.py
@@ -4,3 +4,9 @@ import wordlist
 import stages
 from colorama import init, Fore
 
+# Initialize colorama
+init(autoreset=True)
+
+def display_welcome():
+    print(hangmanintro.hintro[0])
+    print(Fore.CYAN + "Welcome to Hangman!")
