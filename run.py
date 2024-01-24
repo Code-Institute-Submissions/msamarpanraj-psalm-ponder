11751c2939d10ec722d1223fd2825d562ab62991
msamarpanraj <>
2024-01-24 21:45:23 +0000

    update restart game function

--- run.py
+++ run.py
@@ -9,9 +9,22 @@ init(autoreset=True)
 
 def display_welcome():
     print(hangmanintro.hintro[0])
-    
+
 def display_instructions():
     print(Fore.RESET)
     print("1. Start the game")
     print("2. Instructions")
     print("3. Exit the game")
+
+def restart_game():
+    while True:
+        print(Fore.CYAN + "\nDo you want to restart the game?")
+        print("1. Yes")
+        print("2. No")
+        choice = input("\nEnter your choice (1 or 2): ")
+
+        if choice == '1' or choice == '2':
+            return choice == '1'
+        else:
+            print(Fore.RED + "Invalid choice. Please enter either 1 or 2.")
+    
