d11278fd93f3c80e6618da058e36f821ac9edb5a
msamarpanraj <>
2024-01-24 21:41:08 +0000

    update display instruction function to run py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import random
-
 from colorama import init, Fore
 from hangmanintro import hintro
 from wordlist import word_list
@@ -10,4 +9,9 @@ init(autoreset=True)
 
 def display_welcome():
     print(hangmanintro.hintro[0])
-    print(Fore.CYAN + "Welcome to Hangman!")
+    
+def display_instructions():
+    print(Fore.RESET)
+    print("1. Start the game")
+    print("2. Instructions")
+    print("3. Exit the game")
